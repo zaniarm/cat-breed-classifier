97495bfd36c2ca27b26d7c17d00f3c7046c2b2a7
Zaniar Mohammadi <zaniar.mohammadi@gmail.com>
2023-10-12 01:39:28 +0300

    created simple train loop

--- src/main.py
+++ src/main.py
@@ -1,10 +1,12 @@
 import hydra
+
 from config_classes import CatBreedClassifierConfig
+from train import train
 
 
 @hydra.main(config_path="../config", config_name="main", version_base=None)
 def main(config: CatBreedClassifierConfig) -> None:
-    ...
+    train(config)
 
 
 if __name__ == "__main__":
